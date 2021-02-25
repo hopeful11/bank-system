2c1f4d086cd5c557de4aa5bc074ea8e20626d1f2
ttrtahir <ttrtahir@gmail.com>
2021-02-25 17:12:17 +0100

    end of zoom

--- deneme.py
+++ deneme.py
@@ -1,1 +1,35 @@
-testing brach
+class Account:
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+        self.balance = 0
+
+    def __del__(self):
+        pass
+
+    def withdraw(self,amount):
+        self.balance -= amount
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def get_balance(self):
+        print(self.balance)
+
+    def transfer(self, reciever, amount):
+        self.balance -= amount
+        reciever.balance += amount
+
+    def change_account_information(self):
+        pass
+    def delete_account(self):
+        self.__del__()
+    def return_to_main_menu(self):
+        pass
+
+a = Account("mary", 1556)
+b = Account("ahmet", 5445)
+a.deposit(500)
+a.transfer(b,200)
+a.delete_account()
+a.get_balance()
