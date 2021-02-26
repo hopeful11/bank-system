49c3caddb7a2c2b40c90388421c4d40f5a207a21
ttrtahir <ttrtahir@gmail.com>
2021-02-26 13:50:45 +0100

    before 2nd zoom

--- deneme.py
+++ deneme.py
@@ -1,11 +1,25 @@
+class Atm:
+    def __init__(self):
+        self.account_list = []
+        self.account_amount = 0
+
+    def open_a_bank_account(self, name, password):
+        self.account_list.append( Account(name, password, self.account_amount) )
+        self.account_amount += 1
+
+    def login(self, name, password):
+        for obj in self.account_list:
+            if obj.name == name and obj.password == password:
+                return obj
+        return None
+
+
 class Account:
-    def __init__(self, name, password):
+    def __init__(self, name, password, id):
         self.name = name
         self.password = password
+        self.id = id
         self.balance = 0
-
-    def __del__(self):
-        pass
 
     def withdraw(self,amount):
         self.balance -= amount
@@ -20,16 +34,17 @@ class Account:
         self.balance -= amount
         reciever.balance += amount
 
-    def change_account_information(self):
-        pass
+    def change_account_information(self, name, password):
+        self.name = name
+        self.password = password
+
     def delete_account(self):
-        self.__del__()
+        pass
+
     def return_to_main_menu(self):
         pass
 
-a = Account("mary", 1556)
-b = Account("ahmet", 5445)
-a.deposit(500)
-a.transfer(b,200)
-a.delete_account()
-a.get_balance()
+a = Atm()
+a.open_a_bank_account("mary","123pasd123")
+b = a.login("mary", "123pasd123")
+print(b.name)
