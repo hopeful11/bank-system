79a1228b3769ef829fac7fa01e31cbc5abae7bd6
ttrtahir <ttrtahir@gmail.com>
2021-03-02 12:52:27 +0100

    data storing is working

--- deneme.py
+++ deneme.py
@@ -1,21 +1,21 @@
 class Atm:
-    def __init__(self, account_file):
-        self.account_list = []
-        self.account_file = account_file
+    def __init__(self, account_list):
+        self.account_list = account_list
         self.loop()
 
     def open_a_bank_account(self):
         name = input("Enter your name: ")
         password = input("Enter your password: ")
         self.account_list.append( Account(name, password) )
-        self.account_file.write(f"{name},{password}\n")
 
     def login(self):
         name = input("Enter your name: ")
         password = input("Enter your password: ")
         for obj in self.account_list:
             if obj.name == name and obj.password == password:
-                obj.loop()
+                is_del = obj.loop(account_list)
+                if is_del == True:
+                    account_list.remove(obj)
 
     def loop(self):
         while(1):
@@ -29,20 +29,15 @@ class Atm:
             elif temp == '2':
                 self.login()
             elif temp == '3':
-                self.exit_atm()
+                return 0
             else:
                 pass
-        pass
-
-    def exit_atm(self):
-        print("CyA")
-        exit()
 
 class Account:
-    def __init__(self, name, password):
+    def __init__(self, name, password, balance):
         self.name = name
         self.password = password
-        self.balance = 0
+        self.balance = balance
 
     def withdraw(self):
         amount = int(input("Set the amount: "))
@@ -50,15 +45,21 @@ class Account:
 
     def deposit(self):
         amount = int(input("Set the amount: "))
-        self.balance += amount
+        self.balance = str(int(self.balance) + amount)
 
     def get_balance(self):
         print(self.balance)
-        input()
 
-    def transfer(self, reciever, amount):   ###need fix
-        self.balance -= amount
-        reciever.balance += amount
+    def transfer(self):   ###need fix
+        name = input("Who would you like to transfer to: ")
+        amount = int(input("Set the amount: "))
+
+        for obj in self.account_list:
+            if obj.name == name:
+                self.balance = str(int(self.balance) - amount)
+                obj.balance = str(int(obj.balance) + amount)
+                return
+
 
     def change_account_information(self):
         name = input("Enter your new name: ")
@@ -66,14 +67,8 @@ class Account:
         self.name = name
         self.password = password
 
-    def delete_account(self):   ###need fix
-        self.__del__(self)
-        return
-
-    def return_to_main_menu(self):
-        return 0
-
-    def loop(self):
+    def loop(self, account_list):
+        self.account_list = account_list
         while(1):
             temp = input("""This is your account:
             1) withdraw
@@ -81,7 +76,8 @@ class Account:
             3) get balance
             4) transfer money
             5) change account information
-            6) delete account""")
+            6) delete account
+            7) return to main menu""")
             if temp == '1':
                 self.withdraw()
             elif temp == '2':
@@ -93,10 +89,43 @@ class Account:
             elif temp == '5':
                 self.change_account_information()
             elif temp == '6':
-                self.delete_account()
+                return 1
             elif temp == '7':
-                return self.return_to_main_menu()
+                return 0
+
+def set_account_list(account_file):
+    account_list = []
+    for lines in account_file.readlines():
+        account_data = lines.split(",")     #name pass balance
+        account_list.append( Account(account_data[0], account_data[1], account_data[2][:-1]) ) #"\n"
+    return account_list
+
+def end_account_file(account_file, account_list):
+    account_file.seek(0)
+    for i in account_list:
+        account_file.write(f"{i.name},{i.password},{i.balance}\n")
+
 
 if __name__ == "__main__":
+    with open("./accounts.txt", "r+") as account_file:
+        account_list = set_account_list(account_file)
+    Atm(account_list)
     with open("./accounts.txt", "w+") as account_file:
-        a = Atm(account_file)
+        end_account_file(account_file, account_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
